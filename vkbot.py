dd208ffa24cbf8735e4b74ae22548a896ed3b935
514K <alekseysafronov0@gmail.com>
2019-01-27 01:48:18 +0300

    'sadg'

--- vkbot.py
+++ vkbot.py
@@ -1,31 +1,35 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
 def write_msg(user_id, message):
-    vk.method('messages.send', {'user_id': user_id, 'message': message})
+    vk.method('messages.send', {'type': 'confirmation', 'group_id': 177329543})
 
-# API-ключ созданный ранее
+
 token = "b76a32e972f618e9b066b0f637bdb33178c9c1bff79ba99620032cb54debea334b0072a2d940363711409"
 
-# Авторизуемся как сообщество
+
 vk = vk_api.VkApi(token=token)
 
-# Работа с сообщениями
+
 longpoll = VkLongPoll(vk)
 
-# Основной цикл
+
 for event in longpoll.listen():
 
-    # Если пришло новое сообщение
+    
     if event.type == VkEventType.MESSAGE_NEW:
     
-        # Если оно имеет метку для меня( то есть бота)
+        
         if event.to_me:
         
-            # Сообщение от пользователя
+            
             request = event.text
             
-            # Каменная логика ответа
-            if request == "привет":
-                write_msg(event.user_id, "Хай")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
+            
+            if request == "hi":
+                write_msg(event.user_id, "hi")
+            elif request == "spok":
+                write_msg(event.user_id, "bye((")
+            elif request == "":
+                write_msg(event.user_id, "bye((")
             else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+                write_msg(event.user_id, "Shut up bitch...")
